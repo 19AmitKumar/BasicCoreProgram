5a1e30f55ea854ec33a713604dbfeaa127d5d8c8
19AmitKumar <ak9978292@gmail.com>
2023-09-03 11:39:07 +0530

    Added:Solution of FlipCoin

---
+++ BasicCoreProgramingAssignment/BasicCoreProgramingAssignment/FlipCoin.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramingAssignment
+{
+    internal class FlipCoin
+    {
+        public void Toss()
+        {
+            Console.WriteLine("Please Enter the numer of flip: ");
+            int flip = Convert.ToInt32(Console.ReadLine());
+
+            int head = 0;
+            int tail = 0;
+            int headCount = 0;
+            int tailCount = 0;
+            double headPercentage;
+            double tailPercentage;
+
+            if (flip > 0)
+            {
+                for (int i = 0; i < flip; i++)
+                {
+                    Random random = new Random();
+                    int coin = random.Next(2);
+                    if (coin == 0)
+                    {
+                        Console.WriteLine("Its Head!!");
+                        head = 1;
+                        headCount += 1;
+                    }
+                    else if (coin == 1)
+                    {
+                        Console.WriteLine("Its Tail!!");
+                        tail = 1;
+                        tailCount += 1;
+                    }
+                }
+                headPercentage = (headCount * 100) / flip;
+                tailPercentage = (tailCount * 100) / flip;
+                Console.WriteLine("Total Head percentage is: "+headPercentage);
+                Console.WriteLine("Total Tail Percentage is: "+tailPercentage);
+            }
+
+        }
+
+    }
+}
---
+++ BasicCoreProgramingAssignment/BasicCoreProgramingAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramingAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Choice:- ");
+            int d = Convert.ToInt32(Console.ReadLine());
+            switch (d)
+            {
+                case 1:
+                    FlipCoin f = new FlipCoin();
+                    f.Toss();
+                    break;
+            }
+            Console.ReadLine();
+        }
+    }
+}
